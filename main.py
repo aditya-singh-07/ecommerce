94fce4131b3f947a6b1e545691416bbc852a16c3
aditya-singh-07 <adityakusumlata@gmail.com>
2021-05-28 13:22:18 +0530

    fixed some UI bug

--- main.py
+++ main.py
@@ -28,9 +28,37 @@ def shopping():
                            )
 
 @main.route('/food')
+@login_required
 def court():
-    return render_template('index.html')
+    apparel_data = Apparels.query.all()
+    apparel_data_by_men = Apparels.query.filter_by(material_type="men").all()
+    apparel_data_by_women = Apparels.query.filter_by(material_type="women").all()
+    apparel_data_by_sport = Apparels.query.filter_by(material_type="sport").all()
+    apparel_data_by_electronic = Apparels.query.filter_by(material_type="electronic").all()
+    return render_template('index.html',
+                           data=apparel_data,
+                           data_type=apparel_data_by_men,
+                           data_type_women=apparel_data_by_women,
+                           data_type_sport=apparel_data_by_sport,
+                           data_type_electronic=apparel_data_by_electronic
+
+                           )
+
 
 @main.route('/movie')
+@login_required
 def movie():
-    return render_template('index.html')
+    apparel_data = Apparels.query.all()
+    apparel_data_by_men = Apparels.query.filter_by(material_type="men").all()
+    apparel_data_by_women = Apparels.query.filter_by(material_type="women").all()
+    apparel_data_by_sport = Apparels.query.filter_by(material_type="sport").all()
+    apparel_data_by_electronic = Apparels.query.filter_by(material_type="electronic").all()
+    return render_template('index.html',
+                           data=apparel_data,
+                           data_type=apparel_data_by_men,
+                           data_type_women=apparel_data_by_women,
+                           data_type_sport=apparel_data_by_sport,
+                           data_type_electronic=apparel_data_by_electronic
+
+                           )
+
